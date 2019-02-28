270f44367ab492a133fada4fc94e8a0a538350a6
natebecker11 <natebecker11@gmail.com>
2019-02-28 14:34:02 -0500

    Add project files.

---
+++ kitchener/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(kitchener.Startup))]
+namespace kitchener
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
