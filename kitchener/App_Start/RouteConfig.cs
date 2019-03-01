3a1e0f45c4a47d5feca75955f0ef2e9f4049f16e
natebecker11 <natebecker11@gmail.com>
2019-03-01 15:10:14 -0500

    initial files

--- kitchener/App_Start/RouteConfig.cs
+++ kitchener/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace kitchener
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Full Inventory",
+                url: "inventory/{secret}/{format}",
+                defaults: new { controller = "Home", action = "FullInv", format = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- kitchener/Controllers/HomeController.cs
+++ kitchener/Controllers/HomeController.cs
@@ -6,13 +6,17 @@ using System.Web.Mvc;
 
 namespace kitchener.Controllers
 {
+
     public class HomeController : Controller
     {
+
+        // Home/Index
         public ActionResult Index()
         {
             return View();
         }
 
+        //Home/About
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -22,8 +26,29 @@ namespace kitchener.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Enter a New Recipe.";
 
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(string message)
+        {
+            ViewBag.Message = "New Recipe Added!";
+            return View();
+        }
+
+        public ActionResult FullInv(string secret, string format)
+        {
+            if (secret != "full")
+                return new HttpStatusCodeResult(403);
+            if (format == "text")
+                return Content("Full INventory");
+            else if (format == "json")
+                return Json(new { password = "Inventory", expires = DateTime.UtcNow.ToShortDateString() },
+                    JsonRequestBehavior.AllowGet);
+            else if (format == "clean")
+                return PartialView();
             return View();
         }
     }
