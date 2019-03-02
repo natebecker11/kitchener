0e294c30dbc8c89c97269ef4fb72f524d841f968
natebecker11 <natebecker11@gmail.com>
2019-03-02 15:50:01 -0500

    built out routes

--- kitchener/App_Start/RouteConfig.cs
+++ kitchener/App_Start/RouteConfig.cs
@@ -14,15 +14,45 @@ namespace kitchener
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Full Inventory",
-                url: "inventory/{secret}/{format}",
+                name: "Add Inventory Stock",
+                url: "inventory/addstock/",
                 defaults: new { controller = "Home", action = "FullInv", format = UrlParameter.Optional }
             );
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
+                name: "Add Recipe",
+                url: "recipe/add/",
+                defaults: new { controller = "Home", action = "FullInv", format = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Choose A Recipe To Cook",
+                url: "recipe/cook/",
+                defaults: new { controller = "Home", action = "FullInv", format = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Cook A Recipe",
+                url: "recipe/cook/{id}",
+                defaults: new { controller = "Home", action = "FullInv", format = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Add Inventory SKU",
+                url: "inventory/addsku/",
+                defaults: new { controller = "Home", action = "FullInv", format = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Generate Shopping List",
+                url: "shopping/list/",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "View/Audit Full Inventory",
+                url: "inventory/full/",
+                defaults: new { controller = "Home", action = "FullInv", format = UrlParameter.Optional }
             );
         }
     }
